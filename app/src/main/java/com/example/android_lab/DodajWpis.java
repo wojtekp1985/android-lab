200687f3844e7a29aaa51ad66dd6b13c1ded5011
wojciech.arkadiusz.1985 <wojciech.arkadiusz.1985@gmail.com>
2019-04-07 20:28:24 +0200

    1. W DodajWpis.java dodano metodę publiczną wyślij 2. zmapowano kontrolkę EditText 3. na zmapowanym obiekcie uruchomiono metodę getText().toString(); 4. Utworzono obiekt intencji 5. Na obiekcie intencji uruchomiono metodę putExtra ("wpis", pole) tworzącą zmienną "wpis" o wartości pole 6. wywołano metodę setResult(RESULT_OK,intencja)  zwracająca jako wynik pracy obiekt typu intencja 7. zakończono działanie Activity funkcją finish(); 8. Przypisnao metodę Wyślij do Buttona (onClick) 9. W pliku MainActivity.java przeciążono metodę onActivity - odbiera dane z formularza i dodaje je do adaptera

--- app/src/main/java/com/example/android_lab/DodajWpis.java
+++ app/src/main/java/com/example/android_lab/DodajWpis.java
@@ -1,7 +1,10 @@
 package com.example.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view)
+    {
+        EditText kontrolka = (EditText)findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
     }
 }
--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -36,4 +36,14 @@ public class MainActivity extends AppCompatActivity {
         Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
+    @Override protected void onActivityResult( int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
