42841039c06db9f28d88f2615452a138f3ddbafa
wojciech.arkadiusz.1985 <wojciech.arkadiusz.1985@gmail.com>
2019-04-07 19:38:15 +0200

    1. Utworzono nowy resource type - menu 2. W folderze menu utworzono main_menu.xml w utworzonym pliku dodano element item 3. W MainActivity.java przeciążono metodę onCreateOptionsMenu, która dodała zasób main_menu do danego activity 4. Utworzono publiczną metodę void nowyWpis przyjmującą jako parametr obiekt typu MenuItem 5. W metodzie nowy wpis dodano wywołanie intencji. 6. Stworzono nową aktywność o nazwie dodajWpis. 7. W aktywności dodano PlainText oraz Button

---
+++ app/src/main/java/com/example/android_lab/DodajWpis.java
@@ -0,0 +1,13 @@
+package com.example.android_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+}
